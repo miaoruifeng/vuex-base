acce3a751fa0fd9722c51a3d1b2b0065419f70d5
miaoruifeng <mrfnet@163.com>
2019-12-07 21:31:36 +0800

    'add'

---
+++ src/components/Add.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h3>当前最新的count值为：{{$store.state.count}}</h3>
+    <button @click="handleBtn1">+1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleBtn1() {
+      this.$store.commit('add')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
---
+++ src/components/Sub.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3>当前最新的count值为：{{count}}</h3>
+    <button @click="handleBtn1">-1</button>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  computed: {
+    ...mapState(['count'])
+  },
+  methods: {
+    handleBtn1() {
+      this.$store.commit('sub')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
